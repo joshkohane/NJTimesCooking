0f2554a623f3f6961d263595521f050eeebb2d1d
joshkohane <joshkohane@gmail.com>
2020-12-07 15:19:34 -0500

    Setup backend user auth

---
+++ frontend/njtimes_cooking.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    // const store = createStore();
+    ReactDOM.render(<h1>IT'S HAPPENING</h1>, root)
+})
