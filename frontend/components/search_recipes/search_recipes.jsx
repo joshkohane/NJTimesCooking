63f190aba186a41bb555acab9411f17d8f248692
joshkohane <joshkohane@gmail.com>
2020-12-16 19:59:38 -0500

    Remove debuggers

--- frontend/components/search_recipes/search_recipes.jsx
+++ frontend/components/search_recipes/search_recipes.jsx
@@ -4,39 +4,26 @@ import RecipeCard from '../home_page/recipe_card';
 class SearchRecipes extends React.Component {
     constructor(props) {
         super(props)
-        // debugger;
         this.state = {
             searches: JSON.parse(localStorage.getItem('theseSearches')),
         }
     }
 
     componentDidMount() {
-        // this.props.search(this.props.match.params.query)
         this.props.clearSearch();
-        // debugger;
-        // this.props.clearSearch();
+        window.scrollTo(0, 0);
     }
 
     componentDidUpdate(prevProps) {
         if (this.props.location.pathname !== prevProps.location.pathname) {
-            // debugger;
             this.props.clearSearch();
-            // this.props.history.push({state: { theseSearches: Object.freeze(this.state.searches), saveThisRecipe: this.props.history.location.state.saveThisRecipe, deleteThisSave: this.props.history.location.state.deleteThisSave, loggedIn: this.props.history.location.state.loggedIn, openModal: this.props.history.location.state.openModal }})
             this.setState({ searches: JSON.parse(localStorage.getItem('theseSearches'))});
+            window.scrollTo(0, 0);
         }
-        // debugger;
     }
 
-    // componentWillUnmount() {
-    //     this.props.history.push({
-    //         state: { theseSearches: Object.freeze(this.state.searches), saveThisRecipe: this.props.saveThisRecipe, deleteThisSave: this.props.deleteThisSave, loggedIn: this.props.loggedIn, openModal: this.props.openModal }
-    //     })
-    // }
-
     render() {
-        let { theseSearches, saveThisRecipe, deleteThisSave, loggedIn, openModal, isModalOpen } = this.props
-        // Object.freeze(theseSearches)
-        // debugger;
+        let { saveThisRecipe, deleteThisSave, loggedIn, openModal } = this.props
         if (this.state.searches.length === 0) return (
             <div className="no-search-results">
                 <h1 className="no-search-results-heading">No recipe results for "{this.props.match.params.query}"</h1>
@@ -44,7 +31,6 @@ class SearchRecipes extends React.Component {
             </div>
 
         );
-        // const thisSearch = Object.assign([], theseSearches)
 
         return (
             <div className="recipe-box-outer-container" >
