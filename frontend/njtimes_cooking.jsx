37a9a4627139cdb775a4ff4479c392285f47b045
joshkohane <joshkohane@gmail.com>
2021-01-26 19:46:23 -0500

    Set up Google Analytics

--- frontend/njtimes_cooking.jsx
+++ frontend/njtimes_cooking.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+    ReactGA.initialize('G-T2K095V92Z');
+    ReactGA.pageview('/homepage');
+}
+
+initializeReactGA();
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
